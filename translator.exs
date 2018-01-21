7003c1a2a642937606e44936468a186a7c2c5f0e
Dave Lively <davelively14@gmail.com>
2018-01-21 09:02:52 -0500

    Added notes

--- translator.exs
+++ translator.exs
@@ -2,6 +2,8 @@ defmodule Translator do
 
   defmacro __using__(_options) do
     quote do
+      # Register a module attribute @locales that will accumulate. We will store
+      # the lexicon each language to be supported in this attribute.
       Module.register_attribute __MODULE__, :locales, accumulate: true, persist: false
 
       import unquote(__MODULE__), only: [locale: 2]
@@ -10,11 +12,13 @@ defmodule Translator do
   end
 
   defmacro __before_compile__(env) do
+    # Placeholder...will implement compile later
     compile(Module.get_attribute(env.module, :locales))
   end
 
   defmacro locale(name, mappings) do
     quote bind_quoted: [name: name, mappings: mappings] do
+      # Adds a locale to the locales module attribute. Accumulates.
       @locales {name, mappings}
     end
   end
